ba748bf996e21d382ea62161b9445707211fa8f0
joao1barbosa <joao.rbarbosa3@gmail.com>
2024-07-08 23:21:04 -0300

    docs: start script

--- config.sh
+++ config.sh
@@ -13,3 +13,12 @@ composer require laravel/sail --dev
 
 ./vendor/bin/sail php artisan migrate
 
+#Configurando frontend
+cd ../frontend
+
+yarn install
+yarn build
+yarn start
+
+# Tudo online digite CTRL+C Para finalizar a execução do frontend
+# Digite o comando 'cd ../backend' depois './vendor/bin/sail down' para parar o backend
