8a34b0e7043ac5b7e1446bd211a3d3ff3b2080d6
joao1barbosa <joao.rbarbosa3@gmail.com>
2024-08-08 19:41:18 -0300

    feature: AddressRequest created

--- backend/app/Http/Requests/AddressRequest.php
+++ backend/app/Http/Requests/AddressRequest.php
@@ -37,7 +37,31 @@ class AddressRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'cep' => 'required|string|max:8',
+            'logradouro' => 'required|string|max:255',
+            'unidade' => 'nullable|string|max:20',
+            'complemento' => 'nullable|string|max:255',
+            'bairro' => 'required|string|max:50',
+            'localidade' => 'required|string|max:50',
+            'uf' => 'required|string|max:2',
+        ];
+    }
+
+    /**
+     * Retorna as mensagens de erro personalizadas para as regras de validação.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'cep.required' => 'O campo cep é obrigatório.',
+            'logradouro.required' => 'O campo logradouro é obrigatório.',
+            'bairro.required' => 'O campo bairro é obrigatório.',
+            'localidade.required' => 'O campo localidade é obrigatório.',
+            'uf.required' => 'O campo uf é obrigatório.',
+            'cep.max' => 'O campo cep deve ter um cep válido.',
+            'uf.max' => 'O campo uf deve ter :max caracteres',
         ];
     }
 }
