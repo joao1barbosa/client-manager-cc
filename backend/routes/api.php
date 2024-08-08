398e4710f6517c5771d8342fbfe3fe45e0dc3dc9
joao1barbosa <joao.rbarbosa3@gmail.com>
2024-08-08 15:33:37 -0300

    refactor: restructuring files and docker-compose

--- backend/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\ClientController;
-use App\Http\Controllers\Api\CardController;
-use App\Http\Controllers\Api\AddressController;
-
-Route::apiResource('clients', ClientController::class);
-Route::apiResource('cards', CardController::class);
-Route::apiResource('addresses', AddressController::class);
--- frontend/src/hooks/useSearch.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { SearchContext } from "@/contexts/SeachContext";
-
-export function useSearch(){
-    return useContext(SearchContext);
-}
